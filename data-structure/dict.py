38595728da74586d39034cfddf3a10505221b8ae
lksengineer <ksaavedrasu@gmail.com>
2023-07-14 21:10:58 +0000

    data structure

---
+++ data-structure/dict.py
@@ -0,0 +1,13 @@
+persona = {
+  "nombre": "Fulanita",
+  "platziRank": 9567,
+  "cursoFavorito": {
+    "nombre": "Básico de Python",
+    "clases": 30,
+    "duracion": "2 horas"
+  }
+}
+
+print(persona["nombre"]) # "Fulanita"
+print(persona["cursoFavorito"]["nombre"]) # "Básico de Python"
+print(persona["platziRank"]) # 9567
---
+++ data-structure/tuple.py
@@ -0,0 +1,5 @@
+print(type(()))
+
+tuple = (2, 2, 7, 3, 5, 6)
+tuple[1] = 1  #Cannot be modified
+print(tuple)
